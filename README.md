44e6237f981aa2c61d5e9850da1f2bab944e0ffe
Salaheddin AbuEin <sabuein@gmail.com>
2022-08-06 19:19:27 +0100

    A new start...

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# ashaam
+# Salaheddin AbuEin
+
+Welcome to my GitHub homepage repository. I will work on updating this bit by bit while learning the best practice.
+
+Peace & love,
+
+Salah.
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,3 @@
-// This is a single-line comment.
-
-'use strict';
-
-
 let today = new Date();
 let formatDate = today.toDateString();
 let selectDateElement = document.getElementById("date");
