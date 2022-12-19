c7074d5164ee60523dcbc5ecc096a8b96a2378df
Salaheddin AbuEin <sabuein@gmail.com>
2022-12-19 01:25:06 +0000

    A bit more and adding a to-do

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,22 @@
-let today = new Date(),
-    dateElement = document.getElementById("date");
+const yallah = () => {
+    let today = new Date(),
+        hour = today.getHours(),
+        dateElement = document.getElementById("date");
 
-try {
-    dateElement.innerHTML = today.toDateString();
-} catch(error) {
-    dateElement.innerHTML = error.message;
+    try {
+        dateElement.innerHTML = today.toDateString();
+        // To-Do, Maybe?
+        // Apply the light or dark theme based on the time of day
+        if (hour >= 8 && hour < 17) {
+            console.log("It's light!");
+        } else {
+            console.log("It's dark!")
+        }
+    } catch (error) {
+        dateElement.innerHTML = error.message;
+    }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    yallah();
+});
